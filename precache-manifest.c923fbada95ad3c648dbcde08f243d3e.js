1e338895dede0dd852323f57fd439aec21d6de63
Jannah June Nieves <jannahjune@gmail.com>
2019-11-03 15:53:11 -0800

    Updates

---
+++ precache-manifest.c923fbada95ad3c648dbcde08f243d3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fcfbb326bc23341c76bf996de067f96",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "8db2347b326ff4a78839",
+    "url": "/Clicky-Game/static/css/main.f6de48f2.chunk.css"
+  },
+  {
+    "revision": "f878325d3e70cd98f149",
+    "url": "/Clicky-Game/static/js/2.b1c2c3c5.chunk.js"
+  },
+  {
+    "revision": "8db2347b326ff4a78839",
+    "url": "/Clicky-Game/static/js/main.fc512c1f.chunk.js"
+  },
+  {
+    "revision": "eabcff367a27d49b89d0",
+    "url": "/Clicky-Game/static/js/runtime-main.ca802268.js"
+  },
+  {
+    "revision": "5a5bcdea97e7768af98e8d3090e5613a",
+    "url": "/Clicky-Game/static/media/njnaruto.5a5bcdea.woff"
+  }
+]);
